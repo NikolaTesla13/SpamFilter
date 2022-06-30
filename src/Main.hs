4272c8202f9c56f86034db7c9ef83f98262a5cea
Asandei Stefan <stefan.asandei@protonmail.com>
2022-06-30 14:05:43 +0300

    reading the dataset

--- src/Main.hs
+++ src/Main.hs
@@ -5,7 +5,10 @@ import Spam
 
 main :: IO ()
 main = do 
+        contents <- readFile "res/train.txt"
+        let trainingData = train contents
         let message = "Hello, World!"
-        if isSpam message 
+
+        if isSpam message trainingData
             then printf "\"%s\" is spam" message
             else printf "\"%s\" is not spam" message
--- src/Spam.hs
+++ src/Spam.hs
@@ -1,4 +1,16 @@
 module Spam where 
 
-isSpam :: String -> Bool
-isSpam message = True
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+train :: String -> Map String Float
+train datasetContents = do 
+                        let dataLines = lines datasetContents
+                        Map.empty
+
+isSpam :: String -> Map String Float -> Bool
+isSpam message trainingData = True
+
+-- Pr(S|W) = Pr(W|S) / (Pr(W|S) + Pr(W|H)) 
+-- P = P1*P2*...*Pn / (P1*P2*...*Pn + (1-P1)*(1-P2)*...*(1-Pn))
+-- https://en.wikipedia.org/wiki/Naive_Bayes_spam_filtering
